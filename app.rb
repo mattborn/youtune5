329b808fda27720ccf249e2fadca1a2db21f2c57
Matthew Born <git@matthewborn.me>
2012-06-01 13:08:18 -0500

    changed app structure + added styles

--- app.rb
+++ app.rb
@@ -1,10 +1,26 @@
 require 'sinatra'
 require 'httparty'
 
-get '/' do
-  haml :index, :format => :html5
+class Youtube
+  include HTTParty
+  format :json
+  
+  def self.search(terms)
+    get('https://gdata.youtube.com/feeds/api/videos?max-results=1', :query => {:q => terms, :alt => 'json'}).body
+  end
 end
 
-get '/api/' do
-  p HTTParty.get('https://gdata.youtube.com/feeds/api/videos', :query => {:q => params[:q], :alt => 'json'}).inspect
+set :haml, :format => :html5
+
+post '/search' do
+  redirect "/#{params[:q]}"
+end
+
+get '/:terms' do
+  @data = Youtube.search(params[:terms])
+  haml :index
+end
+
+get '/' do
+  haml :index
 end
